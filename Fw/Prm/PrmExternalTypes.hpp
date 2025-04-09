4bb57250c417126d23001cb1ac51c56644cad3a0
Brian Campuzano <Brian.P.Campuzano@jpl.nasa.gov>
2025-04-09 16:09:02 -0600

    Added external parameter delgate documentation

--- Fw/Prm/PrmExternalTypes.hpp
+++ Fw/Prm/PrmExternalTypes.hpp
@@ -13,11 +13,35 @@
 
 namespace Fw {
 
+    //! Fw::ParamExternalDelegate is used for parameters that are stored and managed
+    //! externally to the owning F' component.
+    //!
+    //! Fw::ParamExternalDelegate is an abstract base class that defines the interfaces
+    //! needed by the F' component to interact with the externally managed parameter(s)
     class ParamExternalDelegate {
         public:
-            // TODO Add documentation
-            virtual SerializeStatus deserializeParam(const FwPrmIdType id, const ParamValid prmStat, ParamBuffer& buff) = 0;
-            virtual SerializeStatus serializeParam(const FwPrmIdType id, ParamBuffer& buff) = 0;
+
+            //! Deserialize a parameter from a parameter buffer
+            //!
+            //! \param id: Parameter ID of the parameter being deserialized
+            //! \param prmStat: The parameter status of the parameter being deserialized
+            //! \param buff: The buffer contained the serialized parameter
+            //!
+            //! \return: The status of the deserialize operation
+            virtual SerializeStatus deserializeParam(
+                const FwPrmIdType id,
+                const ParamValid prmStat,
+                ParamBuffer& buff) = 0;
+
+            //! Serialize a parameter into a parameter buffer
+            //!
+            //! \param id: Parameter ID of the parameter to serialized
+            //! \param buff: The buffer to serialize the parameter into
+            //!
+            //! \return: The status of the serialize operation
+            virtual SerializeStatus serializeParam(
+                const FwPrmIdType id,
+                ParamBuffer& buff) const = 0;
         };
 
 }  // namespace Fw
