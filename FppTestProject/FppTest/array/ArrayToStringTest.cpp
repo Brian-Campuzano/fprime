6ccef5391915f7f10206cd460d16641e16ec6e3e
Rob Bocchino <bocchino@jpl.nasa.gov>
2026-02-05 11:03:08 -0800

    Revise FppTest (#4659)
    * Revise FppTest
    
    * Revise FppTest array tests
    
    * Add missing file
    
    * Revise FppTest array tests
    
    * Refactor FppTest enum tests
    
    * Add comment
    
    * Revise FppTest enum tests
    
    * Revise FppTest array tests
    
    * Refactor FppTest enum tests
    
    * Refactor FppTest array tests
    
    * Refactor FppTest enum tests
    
    * Revise FppTest enum tests
    
    * Refactor FppTest struct tests
    
    * Revise FppTest struct tests
    
    * Refactor FppTest struct tests
    
    * Revise FppTest struct tests
    
    * Revise FppTest struct tests
    
    * Update fpp version
    
    * Reformat code
    
    * Fix spelling
    
    * Update fpp version

--- FppTestProject/FppTest/array/ArrayToStringTest.cpp
+++ FppTestProject/FppTest/array/ArrayToStringTest.cpp
@@ -12,7 +12,9 @@
 
 #include "FppTest/array/AliasOfArrayAliasAc.hpp"
 #include "FppTest/array/AliasStringArrayAc.hpp"
+#include "FppTest/array/C_AArrayAc.hpp"
 #include "FppTest/array/EnumArrayAc.hpp"
+#include "FppTest/array/SM_AArrayAc.hpp"
 #include "FppTest/array/StringArrayAc.hpp"
 #include "FppTest/array/StructArrayAc.hpp"
 #include "FppTest/array/Uint32ArrayArrayAc.hpp"
@@ -23,16 +25,20 @@
 
 #include <sstream>
 
+namespace FppTest {
+
+namespace Array {
+
 // Test array string functions
 template <typename ArrayType>
 class ArrayToStringTest : public ::testing::Test {
   protected:
-    void SetUp() override { FppTest::Array::setTestVals<ArrayType>(testVals); }
+    void SetUp() override { setTestVals<ArrayType>(testVals); }
 
     typename ArrayType::ElementType testVals[ArrayType::SIZE];
 };
 
-using ArrayTypes = ::testing::Types<Enum, String, Struct, Uint32Array, AliasString, AliasOfArray>;
+using ArrayTypes = ::testing::Types<AliasOfArray, AliasString, Enum, C_A, SM_A, String, Struct, Uint32Array>;
 TYPED_TEST_SUITE(ArrayToStringTest, ArrayTypes);
 
 // Test array toString() and ostream operator functions
@@ -53,3 +59,7 @@ TYPED_TEST(ArrayToStringTest, ToString) {
 
     ASSERT_STREQ(buf1.str().c_str(), buf2.str().c_str());
 }
+
+}  // namespace Array
+
+}  // namespace FppTest
